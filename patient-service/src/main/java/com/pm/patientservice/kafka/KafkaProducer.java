2bdbc48d6bb871e6d23c9264e0d361d193b04e8d
Rohith Gurram <rohith9998@gmail.com>
2026-01-27 11:19:46 -0600

    Adding files

---
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,15 @@
+package com.pm.patientservice.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+
+
+}
